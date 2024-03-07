472773d156c65da66225e4bc277ea3d5ca827ed0
Timothee Regis <registimothee@gmail.com>
2024-03-07 20:25:05 +0100

    rechange email sender

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,9 +73,9 @@ Rails.application.configure do
   # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.default_url_options = { host: "utopyhostel.com" }
   # config.action_mailer.smtp_settings = {
-  #   address:         'smtp.gmail.com',
-  #   port:            587,
-  #   domain:          'gmail.com',
+  #   address:'smtp.gmail.com',
+  #   port: 587,
+  #   domain:'gmail.com',
   #   user_name: ENV['GMAIL_ADDRESS'],
   #   password: ENV['GMAIL_APP_PASSWORD'],
   #   authentication:  'plain',
